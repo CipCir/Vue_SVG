50fef445597ac8f095fedfb1662db18e562ee954
Ramo <ramona.miuta@gmail.com>
2018-12-17 20:08:16 +0200

    add_svgjs

--- src/components/ramo.vue
+++ src/components/ramo.vue
@@ -1,13 +1,16 @@
 <template>
-  <div id="PageContainer">
-      Ramo
+  <div id="PageContainer">Ramo
+    <div id="drawing"></div>
   </div>
- </template>
+</template>
 <script>
+import SVG from "svg.js";
 export default {
-    
-}
+  mounted() {
+    var draw = SVG("drawing").size(300, 300);
+    var rect = draw.rect(100, 100).attr({ fill: "#f06" });
+  }
+};
 </script>
 <style scoped>
-
 </style>  
