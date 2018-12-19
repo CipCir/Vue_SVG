eb00ae59a6bb706d604e8f78ed5ef0beaccf1dc9
ramona <ramona.miuta@ipsos.com>
2018-12-19 18:17:42 +0200

    de la munca

--- src/components/slider.vue
+++ src/components/slider.vue
@@ -27,13 +27,39 @@ import SVG from "svg.js";
 export default {
   data() {
     return {
-      slide_val: 0,
+      slide_val: 50,
+      nr_interval_points: 25,
+      nr_elem: 5,
       canvas: "",
       circle1: "",
       eye1: "",
       eye2: "",
       mouth: "",
-      radial: ""
+      radial: "",
+      mouth_string: [
+        "m160,404q46.66667,-13 75,0",
+        "m155,416q56,-59 143,0",
+        "m167,364q72.51333,38 146,0",
+        "m167,340q90.89,96 183,0",
+        "m209,265q61.29,111.191 190,49.18"
+      ],
+      mouth_array: [],
+      mouth_coef: [],
+      yellow_hex: "#f1da49",
+      eyeb_path1: [
+        "",
+        "m91.6875,212.25q12,-46.51563 57,-38.88416",
+        "m103,202q26.13861,-34.78378 72,-14.10153",
+        "",
+        ""
+      ],
+      eyeb_path2: [
+        "",
+        "m407,210.71424q-11.45395,-46.02454 -54.40625,-38.47364",
+        "m396,202.90678q-25.77557,-34.78379 -71,-14.10153",
+        "",
+        ""
+      ]
     };
   },
   mounted() {
@@ -47,143 +73,212 @@ export default {
       "stroke-width": 23,
       "stroke-linecap": "round"
     });
+    this.eyeb1 = this.canvas
+      .path("m103,202q26.13861,-34.78378 72,-14.10153")
+      .attr({
+        fill: "none",
+        stroke: "#000",
+        "stroke-width": 23,
+        "stroke-linecap": "round"
+      });
+    this.eyeb2 = this.canvas
+      .path("m396,202.90678q-25.77557,-34.78379 -71,-14.10153")
+      .attr({
+        fill: "none",
+        stroke: "#000",
+        "stroke-width": 23,
+        "stroke-linecap": "round"
+      });
+
+    this.init_setup();
 
     this.cursive(this.slide_val);
   },
   methods: {
-    cursive(x) {
-      var s1, s2, s3;
-
-      this.radial = this.canvas.gradient("linear", function(stop) {
-        s1 = stop.at(0, "rgb(255,0,0)");
-        s2 = stop.at(0.5, "#f1da49");
-      });
-
-      var up_val = 0;
-      var color_string = "";
-      var eyex = 0;
-      var eyey = 0;
-      var eyer = 0;
-      var eyer_max = 35;
-      var mouth_unit = 0;
-      var mouth_poz = 0;
-
-      if (x >= 0 && x <= 25) {
-        up_val = 0.5;
-        color_string =
-          (255 - (x * 200) / 25).toFixed(0).toString() +
+    init_setup() {
+      for (var i = 0; i < this.nr_elem; i++) {
+        this.mouth_array[i] = this.create_arrays(this.mouth_string[i]);
+      }
+      for (var i = 0; i < this.nr_elem - 1; i++) {
+        this.mouth_coef[i + 1] = this.create_coef(
+          this.mouth_array[i],
+          this.mouth_array[i + 1],
+          this.nr_interval_points
+        );
+      }
+    },
+    create_arrays(init) {
+      var temp = init
+        .replace("M", "")
+        .replace("m", "")
+        .replace("Q", ",")
+        .replace("q", ",")
+        .replace(" ", ",")
+        .split(",");
+      var temp2 = [];
+      for (var i = 0; i < temp.length; i++) {
+        temp2[i] = parseInt(temp[i]);
+      }
+      return temp2;
+    },
+    create_coef(v1, v2, nr) {
+      var rez = [];
+      for (var i = 0; i < 6; i++) {
+        rez[i] = (v1[i] - v2[i]) / nr;
+      }
+      return rez;
+    },
+    create_plot(x) {
+      var mouth_poz = this.return_unit_pos(x)[0];
+      var mouth_unit = this.return_unit_pos(x)[1];
+      var rez;
+      rez =
+        "m" +
+        parseInt(
+          this.mouth_array[mouth_poz][0] +
+            this.mouth_coef[mouth_poz][0] * mouth_unit
+        ) +
+        "," +
+        parseInt(
+          this.mouth_array[mouth_poz][1] +
+            this.mouth_coef[mouth_poz][1] * mouth_unit
+        ) +
+        "q" +
+        parseInt(
+          this.mouth_array[mouth_poz][2] +
+            this.mouth_coef[mouth_poz][2] * mouth_unit
+        ) +
+        "," +
+        parseInt(
+          this.mouth_array[mouth_poz][3] +
+            this.mouth_coef[mouth_poz][3] * mouth_unit
+        ) +
+        " " +
+        parseInt(
+          this.mouth_array[mouth_poz][4] +
+            this.mouth_coef[mouth_poz][4] * mouth_unit
+        ) +
+        "," +
+        parseInt(
+          this.mouth_array[mouth_poz][5] +
+            this.mouth_coef[mouth_poz][5] * mouth_unit
+        );
+      return rez;
+    },
+    return_unit_pos(x) {
+      var nr_el = this.nr_elem;
+      var interval_len = this.nr_interval_points;
+      for (var i = 0; i < nr_el; i++) {
+        if (x >= i * interval_len && x <= interval_len * (i + 1)) {
+          return [i + 1, interval_len - x + interval_len * i];
+          break;
+        }
+      }
+      return [0, 0];
+    },
+    return_rgb(x) {
+      var rez = "0,0,0",
+        i_red = 0,
+        i_blue = 0;
+      var len = this.nr_interval_points;
+      var temp = x / this.nr_interval_points;
+      var y = 0;
+      if (this.nr_elem == 5) {
+        i_red = 1;
+        i_blue = 2;
+      }
+      if (temp >= i_red - 1 && temp < i_red) {
+        y = len - this.return_unit_pos(x)[1];
+        y = (y * 25) / len;
+        rez =
+          (255 - (y * 200) / len).toFixed(0).toString() +
           "," +
-          parseInt(x * 3 + 25) +
+          parseInt(y * 3 + len) +
           "," +
-          ((x * 200) / 25).toFixed(0).toString();
+          ((y * 200) / len).toFixed(0).toString();
+      }
+      if (temp >= i_blue - 1 && temp < i_blue) {
+        y = x;
+        y = (y * 25) / len;
+        rez =
+          parseInt(100 - y) +
+          "," +
+          parseInt(100 + parseInt(y)) +
+          "," +
+          (200 + parseInt(y)).toString();
+      }
+
+      return rez;
+    },
+    return_up_val(x) {
+      var rez = 0;
+      var fix_int = 1;
+      var decrease_int = 2;
+      var correction_up;
+      if (this.return_unit_pos(x)[0] == fix_int) {
+        rez = 0.75;
+      }
+      if (this.return_unit_pos(x)[0] == decrease_int) {
+        correction_up = Math.min(
+          1 -
+            (this.nr_interval_points - this.return_unit_pos(x)[1]) /
+              this.nr_interval_points,
+          0.75
+        );
+        rez = correction_up - (x - this.nr_interval_points) * 0.02;
+      }
+      return rez;
+    },
+    calc_eyes(x, eyer_max) {
+      var rx = 0,
+        ry = 0,
+        down_1 = 0,
+        down_2 = 0,
+        eyex = 0,
+        eyey = 0,
+        eyer = eyer_max;
+
+      if (this.nr_elem == 5) {
+        down_1 = 1;
+        down_2 = 2;
+      }
+
+      if (this.return_unit_pos(x)[0] == down_1) {
         eyex = (x - 50) * 0.6;
         eyey = (x - 50) * ((25 - x) * 0.05 + 0.6);
         eyer = 20 + parseInt((15 / 25) * x);
-        mouth_unit = 25 - x;
-        mouth_poz = 1;
-      } else if (x > 25 && x <= 50) {
-        up_val = 0.5 - (x - 25) * 0.02;
-        color_string =
-          parseInt(100 - x) +
-          "," +
-          parseInt(100 + parseInt(x)) +
-          "," +
-          (200 + parseInt(x)).toString();
+      }
+      if (this.return_unit_pos(x)[0] == down_2) {
         eyex = (x - 50) * 0.4;
         eyey = (x - 50) * 0.4;
         eyer = eyer_max;
-        mouth_unit = 50 - x;
-        mouth_poz = 2;
-      } else if (x > 50 && x <= 75) {
-        up_val = 0;
-        color_string = "0,0,255";
-        eyex = 0;
-        eyey = 0;
-        eyer = eyer_max;
-        mouth_unit = 75 - x;
-        mouth_poz = 3;
-      } else {
-        up_val = 0;
-        color_string = "0,0,255";
-        eyex = 0;
-        eyey = 0;
-        eyer = eyer_max;
-        mouth_unit = 100 - x;
-        mouth_poz = 4;
+        // console.log("eyer_max", eyer_max, eyer);
       }
-      // console.log(mouth_unit);
-      // m180,360q67,48 143,0
-      // m155,385q97,3 143,0
-      // m155,385q56.97902,3 84,0       1
-      //m155,416q56.97902,-59 84,0      2
+      return [eyex, eyey, eyer];
+    },
+    cursive(x) {
+      var s1,
+        s2,
+        eyer_max = 35;
 
-      //m155,416q56,-59 143,0           2
-      //m163,365q56,85 143,0            3
-      //m163,365q56,53 143,0
-      //m159,366q72.51333,38 146,0
-      //m167,364q72.51333,38 146,0      3
-      //m167,340q90.89,96 183,0         4
-
-      //m209,265q61.29033,111.19231 190,49.18121      5
-
-      var mouth_array = [4];
-      var mouth_coef = [4];
-      mouth_array[1] = [155, 416, 56, -59, 143, 0];
-      mouth_coef[1] = [0, -1.25, 0, 2.5, -1.6, 0];
-
-      mouth_array[2] = [167, 366, 72, 38, 146, 0];
-      mouth_coef[2] = [-0.48, 2, -0.64, -3.88, -0.125, 0];
-
-      mouth_array[3] = [167, 340, 90, 89, 183, 0];
-      mouth_coef[3] = [0, 1, -0.72, -2.32, -1.48, 0];
-
-      mouth_array[4] = [209, 265, 61, 111, 190, 49];
-      mouth_coef[4] = [-1.68, 3, 1.4, -0.6, -0.28, -1.96];
-
-      this.mouth.plot(
-        "m" +
-          parseInt(
-            mouth_array[mouth_poz][0] + mouth_coef[mouth_poz][0] * mouth_unit
-          ) +
-          "," +
-          parseInt(
-            mouth_array[mouth_poz][1] + mouth_coef[mouth_poz][1] * mouth_unit
-          ) +
-          "q" +
-          parseInt(
-            mouth_array[mouth_poz][2] + mouth_coef[mouth_poz][2] * mouth_unit
-          ) +
-          "," +
-          parseInt(
-            mouth_array[mouth_poz][3] + mouth_coef[mouth_poz][3] * mouth_unit
-          ) +
-          " " +
-          parseInt(
-            mouth_array[mouth_poz][4] + mouth_coef[mouth_poz][4] * mouth_unit
-          ) +
-          "," +
-          parseInt(
-            mouth_array[mouth_poz][5] + mouth_coef[mouth_poz][5] * mouth_unit
-          )
-      );
-
-      console.log(this.mouth.plot());
-      s1.update(0, "rgb(" + color_string + ")");
-      s2.update(up_val, "#f1da49", 1);
-
+      this.radial = this.canvas.gradient("linear", function(stop) {
+        s1 = stop.at(0, "rgb(255,0,0)");
+        s2 = stop.at(0.5, this.yellow_hex);
+      });
+      this.mouth.plot(this.create_plot(x));
+      s1.update(0, "rgb(" + this.return_rgb(x) + ")");
+      s2.update(this.return_up_val(x), this.yellow_hex, 1);
       this.radial.from(0.5, 0).to(0.5, 1);
-      // .radius(1.2);
-
       this.circle1.fill(this.radial);
       this.eye1.attr({
-        r: eyer,
-        cx: 110 + 35 + eyex,
-        cy: 200 + eyer_max - eyey
+        r: this.calc_eyes(x, eyer_max)[2],
+        cx: 110 + 35 + this.calc_eyes(x, eyer_max)[0],
+        cy: 200 + eyer_max - this.calc_eyes(x, eyer_max)[1]
       });
       this.eye2.attr({
-        r: eyer,
-        cx: 320 + 35 + 2 * eyex,
-        cy: 200 + eyer_max - eyey
+        r: this.calc_eyes(x, eyer_max)[2],
+        cx: 320 + 35 + 2 * this.calc_eyes(x, eyer_max)[0],
+        cy: 200 + eyer_max - this.calc_eyes(x, eyer_max)[1]
       });
     }
   }
